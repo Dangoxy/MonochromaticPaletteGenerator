410bc1c174a37a18fdf4949c8fb4fb0aa5881899
Dangoxy <ammar3456@yahoo.com>
2024-07-25 06:50:14 +0300

    replaced fix with tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 
 
 <template>
-  <div class="w-full heightFull flex flex-col">
+  <div class="w-full lg:h-screen h-dvh flex flex-col">
     
     <router-view></router-view>
   </div>
@@ -15,14 +15,5 @@
 </script>
 
 <style scoped>
-.heightFull{
-  height: 100vh;
-}
-
-@media screen and (max-width:450px) {
-  .heightFull{
-    height: 100dvh;
-  }
-}
 
 </style>
